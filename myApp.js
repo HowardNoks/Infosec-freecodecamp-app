afea3c5efbc7d2c04b926cd93f6945c655be4961
unknown <tknoks@gmail.com>
2025-02-05 12:50:26 +0200

    commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const express = require('helmet');
+const helmet = require('helmet');
 const app = express();
 
 
@@ -18,45 +18,19 @@ const app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+//module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/_api', api);
+app.use('/..api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`🥦 Useful programmer info security App started on port ${port}`);
 });
+
+module.exports = app;
